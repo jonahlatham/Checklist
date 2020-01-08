84983da8d2654e859fb2fba1e687615aa9b4b834
Jonah Latham <47837589+jonahlatham@users.noreply.github.com>
2020-01-08 16:50:58 -0500

    code

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/build')))
+///////////////////////////////////////////////////////////////
+let id = 0
+let tasks = []
+
+app.get('/*', (req, res)=>{
+    res.sendFile('index.html', {
+        root: path.join(__dirname, "build")
+    })
+})
+
+app.get('/api/tasks', (req, res) => {
+    res.send(tasks)
+})
+
+app.post('/api/tasks', (req, res) => {
+    id++
+    const newItem = { id, task: req.body.task, isComplete: false }
+    tasks.push(newItem)
+    res.send(tasks)
+})
+
+app.put('/api/tasks', (req, res) => {
+    tasks = tasks.map((e) => {
+        if (e.id === req.body.id) {
+            e.isComplete = true
+        }
+        return e
+    })
+    res.send(tasks)
+})
+
+app.delete('/api/tasks', (req, res) => {
+    tasks = tasks.filter((e) => {
+        return e.id === req.query.id
+    })
+    res.send(tasks)
+})
+
+/////////////////////////////////////////////////////////////////
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
+// http://localhost:8080
--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        task: '',
+        newTasks: []
+    }
+
+    componentDidMount() {
+        axios.get('/api/tasks')
+            .then((response) => {
+                this.setState({
+                    newTasks: response.data
+                })
+                console.log(response.data)
+            })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleResponse = (response) => {
+        this.setState({
+            task: '',
+            newTasks: response.data
+        })
+    }
+
+    handleAdd = () => {
+        const body = {
+            task: this.state.task
+        }
+        if (this.state.task.match(/[0-9a-zA-Z]/)) {
+            axios.post('/api/tasks', body)
+                .then(this.handleResponse)
+        } else {
+            alert('Please fill in the text box before submitting.')
+        }
+    }
+
+    handleComplete = (id) => {
+        const body = {
+            id
+        }
+        axios.put('/api/tasks', body)
+            .then(this.handleResponse)
+    }
+
+
+    handleDelete = (id) => {
+        axios.delete(`/api/tasks?id=${id}`)
+            .then(this.handleResponse)
+    }
+
+    render() {
+        const allTasks = this.state.newTasks.map((e) => {
+            return (<div className='newTask' style={e.isComplete !== true ? { 'box-shadow': 'inset 0 0 10px #8c2222' } : { 'box-shadow': 'inset 0 0 10px #1d8520' }}>
+                <div className='taskWords'>{e.id} -- {e.task}</div>
+                <div className='taskButtons'>
+                    <button className='button' onClick={() => { this.handleComplete(e.id) }}>Complete</button>
+                    <button className='button' onClick={() => { this.handleDelete(e.id) }}>Delete</button>
+                </div>
+            </div>)
+        })
+        return (
+            <div className='App'>
+                <h1>Check List</h1>
+                <div className='addNewDiv'>
+                    <input className='taskInput' onChange={this.handleChange} type='text' placeholder='Task' name='task' value={this.state.task} />
+                    <button className='button' onClick={this.handleAdd}>Add</button>
+                </div>
+                <div>
+                    {allTasks}
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
